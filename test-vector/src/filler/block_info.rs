b646d76a869e6099a47d893ab43759a765f8314a
lightsing <light.tsing@gmail.com>
2025-05-29 15:25:45 +0800

    add block_info opcodes

--- test-vector/src/filler/block_info.rs
+++ test-vector/src/filler/block_info.rs
@@ -1,5 +1,20 @@
-use crate::TestCaseBuilder;
+use crate::{TestCaseBuilder, filler::random_stack_io};
 use revm_bytecode::OpCode;
 use std::{collections::BTreeMap, sync::Arc};
 
-pub(super) fn fill(map: &mut BTreeMap<OpCode, Arc<TestCaseBuilder>>) {}
+pub(super) fn fill(map: &mut BTreeMap<OpCode, Arc<TestCaseBuilder>>) {
+    [
+        OpCode::COINBASE,
+        OpCode::TIMESTAMP,
+        OpCode::NUMBER,
+        OpCode::DIFFICULTY,
+        OpCode::GASLIMIT,
+        OpCode::CHAINID,
+        OpCode::BASEFEE,
+        OpCode::BLOBBASEFEE,
+    ]
+    .into_iter()
+    .for_each(|op| {
+        map.insert(op, random_stack_io(op));
+    });
+}
